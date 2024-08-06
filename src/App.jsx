2753bdfee0e736d8a950544cabd1719f507eb9d0
Julian Usma Giraldo <jeusmag86@gmail.com>
2024-08-06 16:41:49 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import TextComponent from './components/TextComponent/TextComponent';
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <TextComponent />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/TextComponent/TextComponent.jsx
@@ -0,0 +1,107 @@
+function TextComponent() {
+  return (
+    <div>
+      <h1 style={{ textAlign: 'center', color: '#5CBEC5' }}>
+        POLÍTICA DE PRIVACIDAD DE NOHOL
+      </h1>
+      <p style={{ color: '#5CBEC5' }}>
+        <b>POLÍTICA DE PRIVACIDAD SIMPLIFICADO</b>
+      </p>
+      <p style={{ color: '#ffffff' }}>
+        La presente{' '}
+        <b style={{ color: '#5CBEC5' }}>POLÍTICA DE PRIVACIDAD SIMPLIFICADO</b>{' '}
+        se emite en cumplimiento a lo dispuesto por el artículo 23 de la Ley
+        General de Protección de Datos Personales en Posesión de Sujetos
+        Obligados y artículo 24 Ley de Protección de Datos Personales en
+        Posesión de Sujetos Obligados para el Estado de Quintana Roo, y se pone
+        a disposición de los usuarios que entreguen datos o información de
+        carácter personal a través de la{' '}
+        <b style={{ color: '#5CBEC5' }}>APP NOHOL</b>.
+      </p>
+      <p style={{ color: '#ffffff' }}>
+        El H. Ayuntamiento de Othón P. Blanco, informa que es el responsable de
+        Tratamiento de los Datos Personales proporcionados por los Usuarios de
+        la <b style={{ color: '#5CBEC5' }}>APP NOHOL</b>.
+      </p>
+
+      <p style={{ color: '#5CBEC5' }}>
+        <b>POLÍTICA DE PRIVACIDAD INTEGRAL</b>
+      </p>
+      <p style={{ color: '#ffffff' }}>
+        El H. Ayuntamiento de Othón P. Blanco, con domicilio en la avenida
+        Álvaro Obregón, número 321, colonia Centro, código postal 77000, de la
+        Ciudad de Chetumal, Quintana Roo, informa que es el responsable del
+        tratamiento de los Datos Personales que proporcionen los Usuarios de la{' '}
+        <b style={{ color: '#5CBEC5' }}>APP NOHOL</b>, los cuales serán
+        protegidos de conformidad a lo dispuesto por la Ley General de
+        Protección de Datos Personales en Posesión de los Sujetos Obligados, la
+        Ley de Protección de Datos Personales en Posesión de Sujetos Obligados
+        para el Estado de Quintana Roo y demás normatividad que resulte
+        aplicable.
+      </p>
+      <p style={{ color: '#5CBEC5' }}>
+        <b>¿Qué datos personales se recaban y para qué finalidad?</b>
+      </p>
+      <p style={{ color: '#ffffff' }}>
+        Los datos personales que el H. Ayuntamiento de Othón P. Blanco recabará
+        de los Usuarios de la <b style={{ color: '#5CBEC5' }}>APP NOHOL</b> son:
+        Nombre(s), apellidos paterno y materno, fecha de nacimiento,
+        nacionalidad, correo electrónico, número teléfono personal, __________ y
+        __________. Datos necesarios para consultar la información de utilidad
+        para el usuario, generar estadísticas y mejorar el servicio.
+      </p>
+      <p style={{ color: '#5CBEC5' }}>
+        <b>Fundamento para el tratamiento de datos personales</b>
+      </p>
+      <p style={{ color: '#ffffff' }}>
+        El H. Ayuntamiento de Othón P. Blanco trata los datos personales antes
+        señalados con fundamento en el artículo 23 de la Ley General de
+        Transparencia y Acceso a la Información Pública, artículo 2 párrafo
+        segundo y tercero y artículo 23 de la Ley General de Protección de Datos
+        Personales en Posesión de Sujetos Obligados.
+      </p>
+      <p style={{ color: '#5CBEC5' }}>
+        <b>Transferencia de datos</b>
+      </p>
+      <p style={{ color: '#ffffff' }}>
+        El H. Ayuntamiento de Othón P. Blanco informa a los Usuarios de la{' '}
+        <b style={{ color: '#5CBEC5' }}>APP NOHOL</b>:
+      </p>
+      <ul>
+        <li style={{ color: '#ffffff' }}>
+          Que Tratará sus datos personales únicamente por el tiempo que sea
+          necesario con el objetivo de cumplir con las finalidades descritos en
+          la presente política de Privacidad y/o de conformidad con lo que
+          establezcan las disposiciones legales aplicables.
+        </li>
+        <li style={{ color: '#ffffff' }}>
+          Que sus datos personales no serán compartidos a terceras personas,
+          salvo aquellas que sean requeridas por mandamiento judicial.
+        </li>
+      </ul>
+      <p style={{ color: '#5CBEC5' }}>
+        <b>
+          ¿Dónde se pueden ejercer los derechos de acceso, rectificación
+          corrección y oposición de datos personales?
+        </b>
+      </p>
+      <p style={{ color: '#ffffff' }}>
+        Usted podrá ejercer sus derechos de acceso, rectificación, cancelación y
+        oposición de sus datos personales (Derechos ARCO), directamente ante la
+        Unidad de Transparencia y Acceso a la Información, ubicada en la avenida
+        Álvaro Obregón, número 321, colonia Centro, Código Postal 77000, de la
+        Ciudad de Chetumal, Quintana Roo, o mediante correo electrónico
+        transparenciaopb@gmail.com
+      </p>
+      <p style={{ color: '#ffffff' }}>
+        En caso de que exista un cambio en este política de Privacidad, podrá
+        consultarlo a través del sitio web:{' '}
+        <a href='http://www.opb.gob.mx/portal/avisos-privacidad'>
+          http://www.opb.gob.mx/portal/avisos-privacidad
+        </a>
+      </p>
+    </div>
+  );
+}
+
+export default TextComponent;
